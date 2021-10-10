7897fd1ccee2046f6a7c735b58ccc0a9b5d613be
Omer Marquez <omer.marquezt@gmail.com>
2021-10-10 01:51:18 -0400

    ?

--- src/controllers/devices/devices.ts
+++ src/controllers/devices/devices.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express"
 import { executeCameraScriptSync } from "../../helpers/python_run"
 
 export const imageGenerator = (req: Request, res: Response) => {
-  const result = executeCameraScriptSync("~/images/img.jpg")
-  if (result) return res.status(200).sendFile("~/images/img.jpg")
+  const result = executeCameraScriptSync("img.jpg")
+  if (result) return res.status(200).sendFile("img.jpg")
   else
     return res.status(500).json({
       ok: false,
