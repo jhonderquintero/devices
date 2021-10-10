00f0b0e3756af8377aae3b9f285d63dc633020b1
Omer Marquez <omer.marquezt@gmail.com>
2021-10-10 01:56:46 -0400

    path fix

--- src/controllers/devices/devices.ts
+++ src/controllers/devices/devices.ts
@@ -3,7 +3,7 @@ import { executeCameraScriptSync } from "../../helpers/python_run"
 
 export const imageGenerator = (req: Request, res: Response) => {
   const result = executeCameraScriptSync("img.jpg")
-  if (result) return res.status(200).sendFile("img.jpg")
+  if (result) return res.status(200).sendFile("./img.jpg")
   else
     return res.status(500).json({
       ok: false,
